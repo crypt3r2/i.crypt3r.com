6ec3beaa4d8c06eb879128d1e8ef01daafc27d51
crypt3r <mail@crypt3r.com>
2018-07-27 11:10:43 +0200

    deleted breadcrumb.php from /modules/
    Not Needed.

--- modules/breadcrumb.php
+++
@@ -1,8 +0,0 @@
-			<ol class="breadcrumb">
-			  <li><a href="/">Home</a></li>
-			  <li><a href="/blog">Blog</a></li>
-			  <li><a href="/posts">Posts</a></li>
-			  <li class="active">awardcraft</li>
-			</ol>
-		</div>
-	</div>
